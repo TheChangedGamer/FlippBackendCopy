5ae7e99fdad4ed24aa80d230b28fd753519c1319
unknown <nhunguyentran.design@gmail.com>
2020-11-02 17:36:51 -0600

    Backend project for FLipp copy

---
+++ src/main/java/com/FlippCopyBackend/FlippCopy/FlippCopyApplication.java
@@ -0,0 +1,13 @@
+package com.FlippCopyBackend.FlippCopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlippCopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlippCopyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FlippCopyBackend/FlippCopy/FlippCopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FlippCopyBackend.FlippCopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlippCopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
